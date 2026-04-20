955bd31ed38ae70cd046dbc7e7d49f6bcda5c381
Gansons <gansonsraivis@gmail.com>
2026-04-20 21:47:42 +0300

    3uzdevumi

--- funkcijas.php
+++ funkcijas.php
@@ -56,29 +56,120 @@
     ?>
     </form>
     
-    <h3>Funkcija, Uzmini skaitli no 1 līdz 10</h3>
-    <form method="POST">
-        <?php
-            function guess_numb($masivs){
+<h3>Uzmini skaitli</h3>
+<form method="POST">
+    <label>Minimālais skaitlis:</label>
+    <input type="number" name="min" required><br><br>
 
-                $minejums = rand(1,10);
+    <label>Maksimālais skaitlis:</label>
+    <input type="number" name="max" required><br><br>
 
-                if($masivs == $minejums){
-                    echo "<p>Skaitlis tika pareizi uzminēts! :O</p>";
-                }else{
-                    echo "<p> Nepareizi skaitlis bija <b>$minejums</b> </p>";
-                }
-            }
-        ?>
-        <input type="text" name="skaitils">
-        <button type="submit" name="minet">Minēt</button>
-    <?php
+    <label>Tavs minējums:</label>
+    <input type="number" name="skaitlis" required><br><br>
 
-         if(isset($_POST["minet"])){
-            $skaitlis = $_POST["skaitils"];
-        }
-        guess_numb($skaitlis);
-     
-    ?>
-    </form>
+    <button type="submit" name="minet">Minēt</button>
+
+<?php
+function guess_numb($min, $max, $minejums){
+    $random = rand($min, $max);
+
+    if($minejums == $random){
+        echo "<p>Pareizi! Tu uzminēji skaitli!</p>";
+    } else {
+        echo "<p>Nepareizi. Pareizais skaitlis bija <b>$random</b></p>";
+    }
+}
+
+if(isset($_POST["minet"])){
+    $min = $_POST["min"];
+    $max = $_POST["max"];
+    $skaitlis = $_POST["skaitlis"];
+
+    // Validācija
+    if($min > $max){
+        echo "<p>Minimālais skaitlis nevar būt lielāks par maksimālo!</p>";
+    } elseif(!is_numeric($skaitlis)){
+        echo "<p>Ievadi derīgu skaitli!</p>";
+    } else {
+        guess_numb($min, $max, $skaitlis);
+    }
+}
+?>
+</form>
+
+<h3>Paroles pārbaude</h3>
+<form method="POST">
+    <label>Parole:</label>
+    <input type="password" name="parole"><br><br>
+
+    <label>Atkārtot paroli:</label>
+    <input type="password" name="parole2"><br><br>
+
+    <button type="submit" name="check">Pārbaudīt</button>
+
+<?php
+function parbaudit_paroli($p1, $p2){
+    if($p1 === $p2){
+        echo "<p>Paroles sakrīt!</p>";
+    } else {
+        echo "<p>Paroles nesakrīt!</p>";
+    }
+}
+
+if(isset($_POST["check"])){
+    $p1 = $_POST["parole"];
+    $p2 = $_POST["parole2"];
+
+    if(empty($p1) || empty($p2)){
+        echo "<p>Ievadi abas paroles!</p>";
+    } else {
+        parbaudit_paroli($p1, $p2);
+    }
+}
+?>
+
+
+
+</form>
+
+<h3>Sveiciens</h3>
+<form method="POST">
+    <label>Vārds:</label>
+    <input type="text" name="vards"><br>
+
+    <label>Uzvārds:</label>
+    <input type="text" name="uzvards"><br>
+
+    <label>Dzimums:</label>
+    <select name="dzimums">
+        <option value="virietis">Vīrietis</option>
+        <option value="sieviete">Sieviete</option>
+    </select><br>
+
+    <button type="submit" name="sveikt">Sveikt</button>
+
+<?php
+function sveiciens($vards, $uzvards, $dzimums){
+    if($dzimums == "virietis"){
+        echo "<p>Labdien, varenais $vards $uzvards!</p>";
+    } else {
+        echo "<p>Labdien, varenā $vards $uzvards!</p>";
+    }
+}
+
+if(isset($_POST["sveikt"])){
+    $vards = $_POST["vards"];
+    $uzvards = $_POST["uzvards"];
+    $dzimums = $_POST["dzimums"];
+
+    if(empty($vards)){
+        echo "<p>Ievadi vārdu!</p>";
+    } else {
+        sveiciens($vards, $uzvards, $dzimums);
+    }
+}
+?>
+</form>
+
+
 </main>
